9010cb79c45613a81ceae2fb80abd440ff208b98
Ricardo <andrericardoweb@gmail.com>
2021-05-31 12:34:21 -0300

    adicionado menu toolbar - frontend

--- app/src/main/java/com/andrericardoweb/santanderdevweek/MainActivity.kt
+++ app/src/main/java/com/andrericardoweb/santanderdevweek/MainActivity.kt
@@ -2,12 +2,18 @@ package com.andrericardoweb.santanderdevweek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
 
-        
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflate = menuInflater
+        inflate.inflate(R.menu.main_menu, menu)
+        return true
     }
 }
